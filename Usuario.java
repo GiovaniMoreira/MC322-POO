75de8f43c752acf7aa46ec51995cedd3fb448d45
Jpnovaes15 <141690532+Jpnovaes15@users.noreply.github.com>
2023-08-07 22:01:24 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,14 @@
+
+public class Usuario {
+
+	private int RA;
+	private int emprestimo;
+	private String nome;
+
+		// TODO Auto-generated method stub
+	public Usuario(int RA, String nome) {
+		this.RA = RA;
+		this.nome = nome;
+	}
+
+}
