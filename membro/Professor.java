6a19920269333f18cbbbf2e1670896075f057a4c
GiovaniMoreira <giovani.msilva42@gmail.com>
2023-08-27 17:12:17 -0300

    abuble

---
+++ membro/Professor.java
@@ -0,0 +1,14 @@
+
+package membro;
+
+public class Professor extends Usuario {
+ 
+    public Professor(String nome, int ra) {
+        super(nome, ra);
+        this.nivel = 3;
+    }
+
+    // Métodos específicos para Estudante de Pós-Gradutação
+
+    // ...
+}
