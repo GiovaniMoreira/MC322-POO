e4e74c2c4736ee2bc26c7352283a95b3d9d97b2d
GiovaniMoreira <giovani.msilva42@gmail.com>
2023-08-27 16:25:01 -0300

    separando

---
+++ membro/EstudanteGraduacao.java
@@ -0,0 +1,10 @@
+package.class membro;
+
+public class EstudanteGraduacao extends Usuario {
+    public EstudanteGraduacao(String nome, int ra) {
+        super(nome, ra);
+    }
+
+    // Métodos específicos para Estudante de Graduação
+    // ...
+}
---
+++ membro/EstudantePosGraduacao.java
@@ -0,0 +1,11 @@
+package.class membro;
+
+public class EstudantePosGraduacao extends Usuario {
+    public EstudantePosGraduacao(String nome, int ra) {
+        super(nome, ra);
+    }
+
+    // Métodos específicos para Estudante de Pós-Graduação
+    // ...
+}
+s
