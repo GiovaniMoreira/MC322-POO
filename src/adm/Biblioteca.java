12550b04a4b9ebd925b862848f2473a71f2dae66
João Pedro <j174494@dac.unicamp.br>
2023-08-31 16:41:39 -0300

    Prototipo Relatorio e mudanças correlatas

--- src/adm/Biblioteca.java
+++ src/adm/Biblioteca.java
@@ -1,17 +1,16 @@
 package src.adm;
 import java.util.ArrayList;
 import src.Item.*;
-import src.membro.Usuario;
 
 public class Biblioteca {
     private static int data; //Atributo que guarda a data atual, ainda precisamos estudar como implementar
-    private ArrayList<Item> estoque; // Uma relação de agregação entre os itens armazenados na biblioteca e a biblioteca em si
+    private ArrayList<Lista> listas; // Uma relação de agregação entre os itens armazenados na biblioteca e a biblioteca em si
     private ArrayList<Emprestimo> emprestimos; // Esse vetor permite ter um controle também dos emprestimos pela biblioteca ao inves de em cada usuario
     private int multas; //Esse atributo representa apenas quantos usuarios estão multados no momento
 
     public Biblioteca(){
         this.data = 0; //essa data eventualmente substituida por um clock interno ou algo que perceba que tempo é
-        this.estoque = new ArrayList<Item>();
+        this.listas = new ArrayList<Lista>();
         this.emprestimos = new ArrayList<Emprestimo>();
     }
 
@@ -23,18 +22,11 @@ public class Biblioteca {
         this.data = data;
     }
 
-    public ArrayList<Item> getEstoque(){return estoque;}
+    public ArrayList<Lista> getListas(){return listas;}
     public ArrayList<Emprestimo> getEmprestimos(){return emprestimos;}
 
     public int getMultas(){ return multas;}
 
     public void setMultas(int multas){this.multas = multas;}
 
-
-    //Funções de utilidade, possivelmente mudar para os objetos correspondentes
-    public void emprestar(Item item, Usuario usuario){
-        usuario.getEmprestimos().add(new Emprestimo(item, usuario));
-        this.emprestimos.add(new Emprestimo(item, usuario));
-        this.estoque.remove(item);
-    }
 }
--- src/adm/Emprestimo.java
+++ src/adm/Emprestimo.java
@@ -9,13 +9,6 @@ public class Emprestimo {
     //Construtor
     public Emprestimo(Item item, Usuario usuario) {
         this.dataEmprestimo = Biblioteca.getData();
-        if (usuario.getNivel() == 3) {
-            this.dataDevolucao = dataEmprestimo +21;
-        } else if (usuario.getNivel() ==2) {
-            this.dataDevolucao = dataDevolucao +14;
-        }else {
-            this.dataDevolucao = dataEmprestimo +7;
-        }
         this.item = item;
         this.usuario = usuario;
     }
@@ -53,13 +46,12 @@ public class Emprestimo {
     public void devolver(Biblioteca biblioteca){
         int data = biblioteca.getData();
         if (data > this.dataDevolucao){
-            int multa = this.usuario.getMulta();
-            this.usuario.setMulta(multa + (data-dataDevolucao)*3);
+            double multa = this.usuario.getValorMulta();
+            this.usuario.setValorMulta(multa + (data-dataDevolucao)*3);
             biblioteca.setMultas(biblioteca.getMultas()+1);
         }
         biblioteca.getEmprestimos().remove(0);
         usuario.getEmprestimos().remove(0);
-        item.guardar(biblioteca);
     }
 
 }
