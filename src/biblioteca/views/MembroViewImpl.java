4f4fa27dd0f25f41a85ae5f966689a5ee1f0023d
Giovani Moreira <giovani.msilva42@gmail.com>
2023-09-18 21:40:50 -0300

    ListandoMembros

--- src/biblioteca/views/MembroViewImpl.java
+++ src/biblioteca/views/MembroViewImpl.java
@@ -14,6 +14,11 @@ public class MembroViewImpl implements MembroView {
 
     @Override
     public void mostrarListaMembros(List<Membro> membros) {
+        membros.forEach((temp)->{ 
+            System.out.println(temp);
+            System.out.println("--------------");
+        })
+
         // Implementação da exibição da lista de membros
     }
 
