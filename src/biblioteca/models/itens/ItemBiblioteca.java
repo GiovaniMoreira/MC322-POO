053935dc1de54b5a2469c134850bbeac6a46862c
Jpnovaes15 <j174494@dac.unicamp.br>
2023-09-21 17:01:40 -0300

    prototipo item biblioteca

--- src/biblioteca/models/itens/ItemBiblioteca.java
+++ src/biblioteca/models/itens/ItemBiblioteca.java
@@ -1,11 +1,36 @@
 package biblioteca.models.itens;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ItemBiblioteca<T extends ItemMultimidia> {
     private T item;
+    private static ListaEmprestimos<T> itensEmprestados;
+    private static ListaReservas<> itensReservados;
+    private static int nItensEmprestados = itensEmprestados.size();
+    private static int nItensReservados = itensReservados.size();
 
     public ItemBiblioteca(T item){
         this.item = item;
     }
 
 
+    public static int getnItensReservados() {
+        return nItensReservados;
+    }
+
+    public static int getnItensEmprestados() {
+        return nItensEmprestados;
+    }
+
+    public static Collection getItensEmprestados(){
+        return itensEmprestados.values();
+    }
+    public static Collection getItensReservados(){
+        return itensReservados.values();
+    }
+    public void emprestar(int idItem, int idMembro){}
+    public void reservar(int idItem, int idMembro){}
+    public void devolver(int idItem, int idMembro){}
 }
